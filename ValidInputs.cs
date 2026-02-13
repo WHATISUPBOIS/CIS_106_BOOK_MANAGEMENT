d9bc108b8d2826796cbfa75f6cf55071cf00cb41
WHATISUPBOIS <whatisupbois22@gmail.com>
2026-02-13 18:06:18 -0500

    Add ValidInputs.cs enum.

---
+++ ValidInputs.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// Enum representing all valid inputs that a user can enter.
+/// </summary>
+public enum ValidInputs
+{
+    DISPLAY_ALL_BOOKS,
+    DISPLAY_BOOK_BY_ID,
+    ADD_BOOK,
+    REMOVE_BOOK,
+    EXIT
+}
